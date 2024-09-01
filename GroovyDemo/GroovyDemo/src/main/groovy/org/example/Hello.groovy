9eb8655b982c0a3ddbbafa20fc69178c476463a4
chandanp11 <39085343+chandanp11@users.noreply.github.com>
2024-09-01 23:46:28 +0530

    checking in first groovy project

---
+++ GroovyDemo/GroovyDemo/src/main/groovy/org/example/Hello.groovy
@@ -0,0 +1,4 @@
+package org.example
+
+println("groovy is awesome")
+print("groovy is dynamic and cool")
---
+++ ProductApp/app/src/main/java/org/example/Product.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Product {
+
+    private String name;
+    private String description;
+    private double price;
+    
+    public Product(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public double getPrice() {
+        return price;
+    }    
+}

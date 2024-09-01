a634e69809ab1246fb9887f4efc213cdc7984450
chandanp11 <39085343+chandanp11@users.noreply.github.com>
2024-09-02 00:02:10 +0530

    committing data types groovy

--- GroovyDemo/GroovyDemo/src/main/groovy/org/example/DataTypes.groovy
+++ GroovyDemo/GroovyDemo/src/main/groovy/org/example/DataTypes.groovy
@@ -1,2 +1,24 @@
 package org.example
 
+def a = 10
+println a
+println a.class
+
+float b = 10.24F
+println b.class
+
+name = "Chandan"
+s = "hello ${name}"
+println(s)
+println s.class
+
+s1 = '''
+All the power is with you
+you can do anything and everything
+'''
+
+println s1
+
+emailPattern = /[a-zA-Z0-9]+([._-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+([.-][0-9a-zA-Z]+)*\.[a-zA-Z]{2,}/
+
+print "testgmail.com" ==~ emailPattern
